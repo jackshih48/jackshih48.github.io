8befd990fb4022a03513546dd9110a781a5637df
jackshih48 <jackshih174@gmail.com>
2025-10-13 18:18:30 +0800

    K&R: update chap1 (2025-10-13)

--- kr_c/code/chap1/1_5_4_PrintWord_ex12.c
+++ kr_c/code/chap1/1_5_4_PrintWord_ex12.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
-# define IN 1 /* inside a word */
-# define OUT 0 /* outside a word */
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
 
 /* Print its input one word per line */
 int main()
 {
+	int c, state;
 	
+	state = OUT;
+	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\t' || c == '\n') {
+			if (state == IN) {
+				state = OUT;
+				putchar('\n');
+			}
+		} else {
+			state = IN;
+			putchar(c);
+		}
+	}
+	if (state == IN)
+		putchar('\n');
 	
 	return 0;
 }
